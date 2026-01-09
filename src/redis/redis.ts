512a6c11bcf881c51105f19827f4a25ce1a7f4e2
Praveen Uppar <praveenuppar718@gmail.com>
2026-01-09 20:57:28 +0530

    debug

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,6 +1,13 @@
-import { Redis } from '@upstash/redis';
-const redis = new Redis({
-  url: 'https://settled-ghost-32735.upstash.io',
-  token: 'AX_fAAIncDJhYWI2YTM3NTBkOTk0YzcxODRkNTFmNmFjNjE2MDI2OHAyMzI3MzU',
-});
-export default redis;
+// import { Redis } from '@upstash/redis';
+// const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// });
+// export default redis;
+
+// import { Redis } from '@upstash/redis';
+// const redis = new Redis({
+//   url: 'https://settled-ghost-32735.upstash.io',
+//   token: 'AX_fAAIncDJhYWI2YTM3NTBkOTk0YzcxODRkNTFmNmFjNjE2MDI2OHAyMzI3MzU',
+// });
+// export default redis;
